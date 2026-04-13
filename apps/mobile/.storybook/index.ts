1821ffb377b94d67d92cb6452822a20e7be17760
vietvohoangnashtech <viet.vohoang@nashtechglobal.com>
2026-04-13 16:09:18 +0700

    Update version and fix storybook

--- apps/mobile/.storybook/index.ts
+++ apps/mobile/.storybook/index.ts
@@ -1,11 +1,15 @@
-import { view } from '@storybook/react-native';
-import './preview';
+import { view } from './storybook.requires';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const StorybookUI = view.getStorybookUI({
   enableWebsockets: true,
   host: 'localhost',
   port: 7007,
   onDeviceUI: true,
+  storage: {
+    getItem: AsyncStorage.getItem,
+    setItem: AsyncStorage.setItem,
+  },
 });
 
 export default StorybookUI;
